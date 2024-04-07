aaa3501fa444c01310f3929c3cbee0b75205a7ee
Nikolaj Schæbel <s220471@dtu.dk>
2024-04-07 16:33:20 +0200

    Changed function naming scheme to snake case

--- src/core/card.c
+++ src/core/card.c
@@ -1,5 +1,9 @@
 #include <core/card.h>
 #include <stdio.h>
+//change the naming to snake_case for entire file
+//change the naming to snake_case for entire file
+//change the naming to snake_case for entire file
+//go
 
 typedef struct card Linked_Card;
 struct card{
@@ -8,8 +12,8 @@ struct card{
     Linked_Card *next;
 };
 
-Linked_Card *createCard(char rank, char suit){
-    if(validateRank(rank) && validateSuit(suit)){
+Linked_Card *create_card(char rank, char suit){
+    if(validate_rank(rank) && validate_suit(suit)){
         Linked_Card *card = (Linked_Card *)malloc(sizeof(Linked_Card));
         card->rank = rank;
         card->suit = suit;
@@ -19,13 +23,13 @@ Linked_Card *createCard(char rank, char suit){
     return NULL;
 }
 
-Linked_Card *createDeck(){
+Linked_Card *create_deck(){
     Linked_Card *deck = NULL;
     char suits[4] = {'S', 'H', 'D', 'C'};
     char ranks[13] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 13; j++){
-            Linked_Card *card = createCard(ranks[j], suits[i]);
+            Linked_Card *card = create_card(ranks[j], suits[i]);
             if(deck == NULL){
                 deck = card;
             }else{
@@ -40,7 +44,7 @@ Linked_Card *createDeck(){
     return deck;
 }
 
-Linked_Card *createDeckFromFile(char *fileName){
+Linked_Card *create_deck_from_file(char *fileName){
     FILE *file = fopen(fileName, "r");
     if(file == NULL){
         return NULL;
@@ -48,7 +52,7 @@ Linked_Card *createDeckFromFile(char *fileName){
     Linked_Card *deck = NULL;
     char rank, suit;
     while(fscanf(file, "%c %c\n", &rank, &suit) != EOF){
-        Linked_Card *card = createCard(rank, suit);
+        Linked_Card *card = create_card(rank, suit);
         if(deck == NULL){
             deck = card;
         }else{
@@ -63,7 +67,7 @@ Linked_Card *createDeckFromFile(char *fileName){
     return deck;
 }
 
-void saveDeckToFile(Linked_Card *deck, char *fileName){
+void save_deck_to_file(Linked_Card *deck, char *fileName){
     FILE *file = fopen(fileName, "w");
     if(file == NULL){
         return;
@@ -75,18 +79,18 @@ void saveDeckToFile(Linked_Card *deck, char *fileName){
     }
     fclose(file);
 }
-char[2] getCard(Linked_Card *card){
+char[2] get_card(Linked_Card *card){
     char[2] cardStr;
     cardStr[0] = card->rank;
     cardStr[1] = card->suit;
     return cardStr;
 }
 
-void printCard(Linked_Card *card){
+void print_card(Linked_Card *card){
     printf("%c%c", card->rank, card->suit);
 }
 
-void destroyDeck(Linked_Card *deck){
+void destroy_deck(Linked_Card *deck){
     Linked_Card *temp = deck;
     while(temp != NULL){
         Linked_Card *next = temp->next;
@@ -95,21 +99,21 @@ void destroyDeck(Linked_Card *deck){
     }
 }
 
-int validateRank(char rank){
+int validate_rank(char rank){
     if(rank == 'A' || rank == '2' || rank == '3' || rank == '4' || rank == '5' || rank == '6' || rank == '7' || rank == '8' || rank == '9' || rank == 'T' || rank == 'J' || rank == 'Q' || rank == 'K'){
         return 1;
     }
     return 0;
 }
 
-int validateSuit(char suit){
+int validate_suit(char suit){
     if(suit == 'S' || suit == 'H' || suit == 'D' || suit == 'C'){
         return 1;
     }
     return 0;
 }
 
-int validateDeck(Linked_Card *deck) {
+int validate_deck(Linked_Card *deck) {
     int ranks[13] = {0};
     int suits[4] = {0};
     Linked_Card *temp = deck;
@@ -174,4 +178,10 @@ int validateDeck(Linked_Card *deck) {
             return 0;
         }
     }
+    return 1;
 }
+
+
+
+
+
