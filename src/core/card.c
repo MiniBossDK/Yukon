c97b8cb175de6eaa5d42301e176bb34de74e7b3f
Nikolaj Schæbel <s220471@dtu.dk>
2024-04-07 15:31:15 +0200

    Added suggested card with validation

--- src/core/card.c
+++ src/core/card.c
@@ -1,8 +1,23 @@
 #include <core/card.h>
 #include <stdio.h>
 
-void add_card(int card_number, int card_pin, int card_balance) {
-    printf("Card number: %d\n", card_number);
-    printf("Card pin: %d\n", card_pin);
-    printf("Card balance: %d\n", card_balance);
+typedef struct card Linked_Card;
+struct card{
+    char rank;
+    char suit;
+    Linked_Card *next;
+};
+
+int validateRank(char rank){
+    if(rank == 'A' || rank == '2' || rank == '3' || rank == '4' || rank == '5' || rank == '6' || rank == '7' || rank == '8' || rank == '9' || rank == 'T' || rank == 'J' || rank == 'Q' || rank == 'K'){
+        return 1;
+    }
+    return 0;
+}
+
+int validateSuit(char suit){
+    if(suit == 'S' || suit == 'H' || suit == 'D' || suit == 'C'){
+        return 1;
+    }
+    return 0;
 }
