81e0bcef37b1ba9f4134e4eb76ae23ac02fa4a5f
Daniel Lerche <daniel.lerche10@gmail.com>
2024-04-10 09:11:28 +0200

    Added implementation for all the missing function for column

--- src/core/column.c
+++ src/core/column.c
@@ -1,1 +1,24 @@
 #include <core/column.h>
+#include <stdlib.h>
+
+char* get_column_name(Column* column) {
+    char column_name[2];
+    column_name[0] = 'C';
+    column_name[1] = column->pos;
+    return (char*) malloc(sizeof(column_name));
+}
+
+Column* create_column(char pos) {
+    Column* column = (Column*) malloc(sizeof(Column));
+    column->pos = pos;
+    column->next = NULL;
+    return column;
+}
+
+void destroy_column(Column* column) {
+    while(column->next != NULL) {
+        LinkedCard* temp = (LinkedCard *) column;
+        column = (Column *) column->next;
+        free(temp);
+    }
+}
