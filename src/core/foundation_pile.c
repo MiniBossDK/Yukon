1376c3db4adad198c02567f1f93114392a39cb6a
TuranTalayhan <tura0015@gmail.com>
2024-04-10 10:54:49 +0200

    get_last_foundation_card function added

--- src/core/foundation_pile.c
+++ src/core/foundation_pile.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include "core/foundation_pile.h"
 
-void create_foundation_pile(char pile_pos) {
+Foundation_Pile* create_foundation_pile(char pile_pos) {
     Foundation_Pile* pile = (Foundation_Pile*)malloc(sizeof(Foundation_Pile));
     pile->pile_pos = pile_pos;
     pile->prev = NULL;
     pile->next = NULL;
+    return pile;
 }
 
 void destroy_foundation_pile(Foundation_Pile* pile) {
@@ -18,4 +19,12 @@ char* get_foundation_pile(Foundation_Pile* pile) {
     str[1] = pile->pile_pos;
     str[2] = '\0';
     return str;
+}
+
+LinkedCard* get_last_foundation_card(Foundation_Pile* pile) {
+    LinkedCard* card = pile->next;
+    while (card->next != NULL) {
+        card = card->next;
+    }
+    return card;
 }
