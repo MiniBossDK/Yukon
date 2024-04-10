2522850c6efe80527e0b1706c23fc9e5ea339b21
Daniel Lerche <daniel.lerche10@gmail.com>
2024-04-10 13:19:16 +0200

    Removed everything from column

--- src/core/column.c
+++ src/core/column.c
@@ -2,37 +2,3 @@
 #include <core/card.h>
 #include <stdlib.h>
 
-char* get_column_name(Column* column) {
-    char column_name[2];
-    column_name[0] = 'C';
-    column_name[1] = column->pos;
-    return (char*) malloc(sizeof(column_name));
-}
-
-Column* create_column(char pos) {
-    Column* column = (Column*) malloc(sizeof(Column));
-    column->pos = pos;
-    column->next = NULL;
-    column->prev = NULL;
-    return column;
-}
-
-void destroy_column(Column* column) {
-    if(column->next == NULL) {
-        LinkedCard *card = column->next;
-        while(card->next != NULL) {
-            card = card->next;
-            destroy_card(card->prev);
-        }
-        destroy_card(card);
-    }
-    free(column);
-}
-
-LinkedCard* get_last_column_card(Column* column) {
-    LinkedCard* temp = column->next;
-    while(temp->next != NULL) {
-        temp = temp->next;
-    }
-    return temp;
-}
--- src/core/include/core/column.h
+++ src/core/include/core/column.h
@@ -1,30 +1,3 @@
 #pragma once
 #include <core/card.h>
 
-typedef struct Column Column;
-
-struct Column {
-    char pos;
-    LinkedCard* next;
-    LinkedCard* prev;
-};
-
-/*
- * Get the textual name of the column.
- */
-char* get_column_name(Column* column);
-
-/*
- * Create a column with the given position.
- */
-Column* create_column(char pos);
-
-/*
- * Destroys the given column and all the cards that it links to.
- */
-void destroy_column(Column* column);
-
-/*
- * Get the last card in the column.
- */
-LinkedCard* get_last_column_card(Column* column);
