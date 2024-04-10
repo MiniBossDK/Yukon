b7d9fd6682c68b470b1a1bf44058aa1dcfd52b32
Nikolaj Schæbel s220471 <s220471@dtu.dk>
2024-04-10 08:50:24 +0200

    Added foundation pile

---
+++ src/core/include/core/foundation_pile.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "card.h"
+
+typedef struct Foundation_Pile Foundation_Pile;
+
+struct Foundation_Pile {
+    char pile_pos;
+    LinkedCard* next;
+};
+
+void create_foundation_pile(char pile_pos);
+
+void destroy_foundation_pile(Foundation_Pile* pile);
+
+char* print_foundation_pile(Foundation_Pile* pile);
