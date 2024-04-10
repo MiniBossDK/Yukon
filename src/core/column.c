479758d962e6e3cdcda842c9c30e92bce15e3275
TuranTalayhan <tura0015@gmail.com>
2024-04-10 13:56:04 +0200

    created functions validate_move_to_foundation and validate_move_to_column

--- src/core/column.c
+++ src/core/column.c
@@ -1,4 +1,9 @@
 #include <core/column.h>
 #include <core/card.h>
-#include <stdlib.h>
 
+int validate_move_to_column(LinkedCard* column_card, LinkedCard* moved_card) {
+    if(column_card == 0) {
+        return moved_card->value == 13;
+    }
+    return (column_card->value - 1 == moved_card->value && column_card->suit != moved_card->suit);
+}
--- src/core/include/core/column.h
+++ src/core/include/core/column.h
@@ -1,2 +1,5 @@
 #pragma once
 
+#include <core/card.h>
+
+int validate_move_to_column(LinkedCard* column_card, LinkedCard* moved_card);
