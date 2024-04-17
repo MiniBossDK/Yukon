07236bf07690ef88011559297c5e2f7032018c76
Asma <s230716@dtu.dk>
2024-04-17 12:05:13 +0200

    Comments in game_init

--- src/main.c
+++ src/main.c
@@ -57,55 +57,76 @@ void free_foundation_piles(LinkedCard *card[4]) {
     }
 }
 */
+
+// game_init Function to initialize the game setup by distributing cards from the deck across seven columns.
 void game_init(LinkedCard *deck, LinkedCard *column[7]) {
 
+    // Make a new deck that is a clone of the original deck.
     LinkedCard* deck_clone = clone_deck(deck);
+    // initialize index, start_index, end_index and hide_index to track column positions and control card visibility.
     int index = 0;
     int start_index = 0;
     int end_index = 6;
     int hide_index = 0;
-
+    // It points to the next card in the deck.
     LinkedCard* temp = deck_clone->next;
+    // Loop through all 52 cards in the deck.
     for (int i = 0; i < 52; i++) {
+        // Check if the current column already contains cards.
         if (column[index] != NULL) {
+            // Link the new card to it.
             LinkedCard* temp2 = column[index];
             temp2 = get_last_card(temp2);
+            // Link the current card from the deck_clone to the last card in the column.
             temp2 -> next = deck_clone;
             deck_clone -> prev = temp2;
             deck_clone->next = NULL;
+            // Hide the card, if the current index is beyond the hide_index.
             if (index > hide_index) {
                 hide_card(deck_clone);
             }
         } else {
+            // If the current column is empty, place the first card there.
             column[index] = deck_clone;
+            // Hide the card if it is not the first column.
             if(index > 0) {
                 hide_card(column[index]);
             }
+            // Initialize the prev and next pointers to NULL.
             column[index] -> prev = NULL;
             column[index] -> next = NULL;
         }
+        //Now we update the next card, and Set the previous pointer of the temp card pointer to NULL.
         temp -> prev = NULL;
         if (temp -> next != NULL) {
             temp -> prev = NULL;
+            // Move the deck_clone pointer to the current position of temp.
             deck_clone = temp;
             temp = deck_clone -> next;
         }
         else {
+            // Set deck_clone to temp, which is the last card.
             deck_clone = temp;
         }
-
+        // Increment the column index to distribute the next card.
         index++;
+
+        // Adjust or changing the starting index at specific card counts
+        // to change the cards distribution sequence in right quantity on every column. .
         if (i == 6 || i == 36 || i == 41 || i == 45 || i == 48 || i == 50 ) {
             start_index++;
         }
+
+        // Reset index to the start_index.
         if (index > end_index) {
             index = start_index;
             hide_index++;
         }
 
     }
+    // set deck_clone to NULL for cleanup.
     deck_clone = NULL;
-    destroy_deck(deck_clone);
+    destroy_deck(deck_clone); // Free the memory allocated for the deck_clone now that the game is set up.
 }
 
 void show_deck(LinkedCard* deck, LinkedCard* column[7]) {
